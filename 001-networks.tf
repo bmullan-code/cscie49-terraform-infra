359f799f569706c9faf6cfde7146b9bba070a2e6
Barry Mullan <bmullan@google.com>
2023-04-22 13:57:40 -0400

    updates

---
+++ 001-networks.tf
@@ -0,0 +1,35 @@
+
+resource "google_compute_network" "vpc_network" {
+  name                    = "${var.prefix}-network"
+  auto_create_subnetworks = "false"
+}
+
+resource "google_compute_subnetwork" "vpc_subnet" {
+    name          = "${var.prefix}-subnet"
+    ip_cidr_range = var.network_subnet_0_cidr
+    region        = var.region
+    network       = google_compute_network.vpc_network.id
+}
+
+resource "google_compute_router" "router" {
+  name    = "${var.prefix}-router"
+  region  = google_compute_subnetwork.vpc_subnet.region
+  network = google_compute_network.vpc_network.id
+
+  bgp {
+    asn = 64514
+  }
+}
+
+resource "google_compute_router_nat" "nat" {
+  name    = "${var.prefix}-nat"
+  router  = google_compute_router.router.name
+  region  = google_compute_router.router.region
+  nat_ip_allocate_option             = "AUTO_ONLY"
+  source_subnetwork_ip_ranges_to_nat = "ALL_SUBNETWORKS_ALL_IP_RANGES"
+
+  log_config {
+    enable = true
+    filter = "ERRORS_ONLY"
+  }
+}
--- variables.tf
+++ variables.tf
@@ -15,10 +15,6 @@ variable "terraform_service_account" {
     type = string
 }
 
-variable "default_service_account" {
-    type = string
-}
-
 variable "network_subnet_0_cidr" {
     type = string
     default = "10.1.0.0/24"
