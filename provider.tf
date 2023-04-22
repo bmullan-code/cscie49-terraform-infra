8cf486fba54744cd55d84bfd03be787b67f389da
Barry Mullan <bmullan@google.com>
2023-04-22 13:47:01 -0400

    first update

---
+++ provider.tf
@@ -0,0 +1,33 @@
+terraform {
+ backend "gcs" {
+   bucket  = "cscie49-terraform-state"
+   prefix  = "terraform/state"
+ }
+}
+
+# cscie49-terraform-state
+provider "google" {
+  project = var.project_id
+  region  = var.region
+  zone    = var.zone
+  alias = "impersonation"
+  scopes = [
+    "https://www.googleapis.com/auth/cloud-platform",
+    "https://www.googleapis.com/auth/userinfo.email",
+  ]
+}
+
+data "google_service_account_access_token" "default" {
+  provider               	= google.impersonation
+  target_service_account 	= var.terraform_service_account
+  scopes                 	= ["userinfo-email", "cloud-platform"]
+  lifetime               	= "1200s"
+}
+
+provider "google" {
+  project = var.project_id
+  region  = var.region
+  zone    = var.zone
+  access_token	= data.google_service_account_access_token.default.access_token
+  request_timeout 	= "60s"
+}
