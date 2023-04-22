782a0d8a43d73b0840e5df7c9a87d866b56f5397
Barry Mullan <bmullan@google.com>
2023-04-22 15:30:32 -0400

    service accounts

--- 000-service-accounts.tf
+++ 000-service-accounts.tf
@@ -2,5 +2,5 @@ resource "google_service_account" "bigquery_scheduled_queries" {
   account_id   = "bigquery-scheduled-queries"
   display_name = "BigQuery Scheduled Queries Service Account"
   description  = "Used to run BigQuery Data Transfer jobs."
-  project = var.target_project
+  project = var.project_id
 }
