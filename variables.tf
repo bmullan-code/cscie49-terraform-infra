dccb608ef13763bc0b3308f96ded8872abf5794a
Barry Mullan <bmullan@google.com>
2023-05-06 13:31:13 -0400

    removed unused variable

--- variables.tf
+++ variables.tf
@@ -32,8 +32,3 @@ variable "gke_num_nodes" {
   default     = 3
   description = "number of gke nodes"
 }
-
-variable "default_service_account" {
-  type = string
-  description = "default service account"
-}
